dcfdae8fd9b3a683c14318ac691e50d023bec1ac
Fastest17 <kavaletsvasia@gmail.com>
2019-11-17 19:58:52 +0200

    end header and footer

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -4,7 +4,51 @@ $(document).ready(function () {
     /* ------------------------------------------- */
     $('#preloader').fadeOut();
 
+    /* ------------------------------------------- */
+    /*                  SUB MENU                   */
+    /* ------------------------------------------- */
+    $('.icon-menu').on('click', function (e) {
+        e.preventDefault();
+        $('.top-bar__menu').slideToggle();
+    });
 
+    /* ------------------------------------------- */
+    /*               ACTIVE MENU                   */
+    /* ------------------------------------------- */
+    $(".top-bar__menu a")
+        .click(function (e) {
+            let link = $(this);
 
+            if (link.hasClass("active")) {
+                link.removeClass("active");
+            } else {
+                link.addClass("active");
+            }
+        })
+        .each(function () {
+            let link = $(this);
+            if (link.get(0).href === location.href) {
+                link.addClass("active").parents("li").addClass("active");
+                return false;
+            }
+        });
+
+    $(".footer__nav-menu a")
+        .click(function (e) {
+            let link = $(this);
+
+            if (link.hasClass("active")) {
+                link.removeClass("active");
+            } else {
+                link.addClass("active");
+            }
+        })
+        .each(function () {
+            let link = $(this);
+            if (link.get(0).href === location.href) {
+                link.addClass("active").parents("li").addClass("active");
+                return false;
+            }
+        });
 
 });

41b4272f0d63b61e45515cd4f38d7459040f9730
Fastest17 <kavaletsvasia@gmail.com>
2019-11-24 22:42:58 +0200

    add banner and products section

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -51,4 +51,40 @@ $(document).ready(function () {
             }
         });
 
+    /* ------------------------------------------- */
+    /*           IMAGE TO BACKGROUND               */
+    /* ------------------------------------------- */
+    function changeImgToBg(imgSel, parentSel) {
+
+        if (!imgSel) {
+            console.info('no img selector');
+            return false;
+        }
+
+        let $parent,
+            _this;
+
+        $(imgSel)
+            .each(function () {
+                _this = $(this);
+                if ('none' == _this.css('display')) {
+                    return true;
+                }
+
+                $parent = _this.closest(parentSel);
+                $parent = $parent.length ? $parent : _this.parent();
+                $parent.css('background-image', 'url(' + _this.attr('src') + ')');
+                _this.hide();
+            });
+    }
+
+    changeImgToBg('.js-bg');
+
+    /* ------------------------------------------- */
+    /*             SEE MORE BUTTON                 */
+    /* ------------------------------------------- */
+    $('.products__see-more-btn').on('click', function() {
+        // $('.products__item-wrap').removeClass('hide');
+        $('.products__item-wrap').slideDown();
+    });
 });
